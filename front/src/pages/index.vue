9f595d09e97b3214b41320d4cb4025dc24e16670
Ermengol B. (Professor) <ebota@inspedralbes.cat>
2024-10-23 23:20:38 +0200

    Ruta bàsica i variable d'entorn

--- front/src/pages/index.vue
+++ front/src/pages/index.vue
@@ -8,13 +8,19 @@
       </v-list>
     </v-navigation-drawer>
 
-    <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
-      Contingut de l'aplicació
+    <v-main class="align-center justify-center" style="min-height: 300px;">
+
+      <h1>Exemple d'aplicació que mostra l'ús de rutes, i també de les variables d'entorn</h1>
+
+      <p> Per veure un <router-link to="/llibre">llibre</router-link> utilitzem una ruta cap a la pàgina que hem creat a
+        pages. Ho fem amb el component <code> &lt;router-link to="page"&gt; </code></p>
+      <p>Per accedir al back fem servir una variable d'entorn que canvia de valor en funció del mode</p>
+      
     </v-main>
   </v-layout>
 
 </template>
 
 <script setup>
-//
+
 </script>
---
+++ front/src/pages/llibre.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>URL del back que canvia en funció del mode: {{ URL_BACK }}</h1>
+    <p>El valor de la variable s'agafa d'un fitxer o un altre en funció del mode (dev, build...)
+
+    </p>
+    <h2>Fitxers possible</h2>
+    <p>El normal és tenir els tres fitxers</p>
+    <ul>
+      <li><code>.env:</code>Hauria de contenir les claus, sense valor. Aquest fitxer SÍ que puja al github</li>
+      <li><code>.env.locat: </code>Serveix per definir l'entorn de desenvolupament. Cada desenvolupador hi posa els seus
+        valors. NO puja al gthub</li>
+      <li><code>.env.production: </code>Es llegeix quan es fa <code>npm run build</code> i per tant, l'aplicació
+        resultant que està a la carpeta dist utilitza aquests valors</li>
+    </ul>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      URL_BACK: import.meta.env.VITE_URL_BACK // Definida En el fitxer .env i/o .env.local, a l'arrel del vue
+    };
+  },
+}
+</script>
+
+<style scoped>
+/* Afegir estils si és necessari */
+</style>
